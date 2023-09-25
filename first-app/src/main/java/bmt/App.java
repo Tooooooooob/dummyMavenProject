e1a92f18b7c3d5dc81ab14248d205f5e03597c87
Tanmay Bhosale <2021.tanmay.bhosale@ves.ac.in>
2023-09-25 12:49:29 +0530

    Committing the App

---
+++ first-app/src/main/java/bmt/App.java
@@ -0,0 +1,13 @@
+package bmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Tanmay's First, Simple Maven Project!" );
+    }
+}
